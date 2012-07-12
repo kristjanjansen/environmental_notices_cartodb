1a0ef950a2308054e1880bc73ce15308332e5555
Kristjan Jansen <kristjan.jansen@gmail.com>
2012-07-12 10:36:31 +0300

    15 min

--- worker.js
+++ worker.js
@@ -250,10 +250,10 @@ function fusion_insert(table_id, row, callback) {
 
 }
 
-// Scheduler to launch a worker in every 5 minutes
+// Scheduler to launch a worker in every 15 minutes
   
 var rule = new schedule.RecurrenceRule();
-rule.minute = new schedule.Range(0, 60, 5);
+rule.minute = new schedule.Range(0, 60, 15);
 
 var j = schedule.scheduleJob(rule, function(){
     console.log('Launching worker');
